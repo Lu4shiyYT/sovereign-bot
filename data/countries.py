f7841bb8f739f298f8a22ddf98f5a7c62453b3a7
Lu4shiyYT <ivan1one1one@gmail.com>
2026-07-23 22:09:41 +0300

    Update countries.py

--- data/countries.py
+++ data/countries.py
@@ -1,15 +1,9 @@
 initial_countries = [
-    {"name": "Сомали", "type": "ужасно развитые", "population": 17_000_000, "budget": 5_000_000, "army": 5000,
-     "religion": "Ислам", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 2},
-    {"name": "Афганистан", "type": "ужасно развитые", "population": 40_000_000, "budget": 8_000_000, "army": 12000,
-     "religion": "Ислам", "government": "Теократия", "ideology": "Национализм", "bot_strength": 3},
-    {"name": "Чад", "type": "ужасно развитые", "population": 17_000_000, "budget": 3_000_000, "army": 4000,
-     "religion": "Ислам", "government": "Президентская республика", "ideology": "Консерватизм", "bot_strength": 2},
-    {"name": "Нигер", "type": "ужасно развитые", "population": 25_000_000, "budget": 4_000_000, "army": 7000,
-     "religion": "Ислам", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 2},
-    {"name": "ЦАР", "type": "ужасно развитые", "population": 5_000_000, "budget": 1_000_000, "army": 2000,
-     "religion": "Христианство", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 1},
-    {"name": "Нигерия", "type": "плохо развитые", "population": 220_000_000, "budget": 40_000_000, "army": 80000,
-     "religion": "Христианство", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 4},
+    {"name": "Сомали", "type": "ужасно развитые", "population": 17_000_000, "budget": 5_000_000, "army": 5000, "religion": "Ислам", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 2},
+    {"name": "Афганистан", "type": "ужасно развитые", "population": 40_000_000, "budget": 8_000_000, "army": 12000, "religion": "Ислам", "government": "Теократия", "ideology": "Национализм", "bot_strength": 3},
+    {"name": "Чад", "type": "ужасно развитые", "population": 17_000_000, "budget": 3_000_000, "army": 4000, "religion": "Ислам", "government": "Президентская республика", "ideology": "Консерватизм", "bot_strength": 2},
+    {"name": "Нигер", "type": "ужасно развитые", "population": 25_000_000, "budget": 4_000_000, "army": 7000, "religion": "Ислам", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 2},
+    {"name": "ЦАР", "type": "ужасно развитые", "population": 5_000_000, "budget": 1_000_000, "army": 2000, "religion": "Христианство", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 1},
+    {"name": "Нигерия", "type": "плохо развитые", "population": 220_000_000, "budget": 40_000_000, "army": 80000, "religion": "Христианство", "government": "Президентская республика", "ideology": "Национализм", "bot_strength": 4},
     # ... остальные страны с добавленными полями
 ]
